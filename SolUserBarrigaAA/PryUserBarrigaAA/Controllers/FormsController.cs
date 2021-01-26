5806d4d5dc86b4cfd26a768184972f78609ce030
AnahiBarriga <a2019119382@estudiantes.upsa.edu.bo>
2021-01-25 19:14:28 -0600

    Cree el controlador

---
+++ SolUserBarrigaAA/PryUserBarrigaAA/Controllers/FormsController.cs
@@ -0,0 +1,119 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PryUserBarrigaAA.Models;
+
+namespace PryUserBarrigaAA.Controllers
+{
+    public class FormsController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Forms
+        [Authorize]
+        public IQueryable<Form> GetForms()
+        {
+            return db.Forms;
+        }
+
+        // GET: api/Forms/5
+        [Authorize]
+        [ResponseType(typeof(Form))]
+        public IHttpActionResult GetForm(int id)
+        {
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(form);
+        }
+
+        // PUT: api/Forms/5
+        [Authorize]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutForm(int id, Form form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != form.PersonId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(form).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FormExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Forms
+        [Authorize]
+        [ResponseType(typeof(Form))]
+        public IHttpActionResult PostForm(Form form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Forms.Add(form);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = form.PersonId }, form);
+        }
+
+        // DELETE: api/Forms/5
+        [Authorize]
+        [ResponseType(typeof(Form))]
+        public IHttpActionResult DeleteForm(int id)
+        {
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return NotFound();
+            }
+
+            db.Forms.Remove(form);
+            db.SaveChanges();
+
+            return Ok(form);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool FormExists(int id)
+        {
+            return db.Forms.Count(e => e.PersonId == id) > 0;
+        }
+    }
+}
---
+++ SolUserBarrigaAA/PryUserBarrigaAA/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace PryUserBarrigaAA.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+       public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<PryUserBarrigaAA.Models.Form> Forms { get; set; }
+    }
+        
+   
+}
